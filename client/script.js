fd12203ec053314a9c3d5bd640c8dd8b55143f39
Michaelan171 <anlong19841013@gmail.com>
2023-03-05 18:42:17 +0800

    update the server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatbot-gpt-4th2.onrender.com', {
     method:'POST',
     headers:{
       'Content-Type': 'application/json'
